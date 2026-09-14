8f409ef7af74d2f7c786cfb56b8e9b4364f21447
AbdelRaffyNatangcop2013 <ardnatangcop@cmc.pshs.edu.ph>
2026-09-14 15:02:11 +0800

    Update K21-1FA05-Natangcop.py
    Changes made:
    -Gave more clearer instructions seen in the code to the user
    -Comments ordered the steps and made it more understandable

--- K21-1FA05-Natangcop.py
+++ K21-1FA05-Natangcop.py
@@ -6,12 +6,14 @@ Without them, manually writing algorithms for powers and square roots would give
 """
 import math
 
+# Step 1: Collect coordinate inputs from the user
 x1 = float(input("Enter x1: "))
 y1 = float(input("Enter y1: "))
 x2 = float(input("Enter x2: "))
 y2 = float(input("Enter y2: "))
 
-
+# Step 2: Calculate the distance between the points using the Euclidean distance formula.
 distance = math.sqrt(math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2))
 
+# Step 3: Display the results
 print(f"\nThe distance between the two points is: {distance:.2f}")
